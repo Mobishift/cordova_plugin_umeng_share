9f08d3befc13a575790e99c8f2a571189b2d298d
Zhengz <zhengz@masn.cn>
2015-06-26 19:40:14 +0800

    js脚本集成

--- www/UMengShare.js
+++ www/UMengShare.js
@@ -1,5 +1,17 @@
+'use strict';
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "UMengShare", "coolMethod", [arg0]);
+var noop = function(){};
+
+var execPlugin = function(action, arg0, success, error) {
+    exec(success, error, 'UMengShare', action, [arg0]);
+};
+
+exports.share = function(shareData, successCallback, errorCallback){
+    successCallback = successCallback || noop;
+    errorCallback = errorCallback || noop;
+    if(!shareData){
+        throw 'please input share data';
+    }
+    execPlugin('share', JSON.stringify(shareData), successCallback, errorCallback);
 };
