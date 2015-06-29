f72dcc7a1f807e11dc41a03a60a1a6633a05a707
Zhengz <zhengz@masn.cn>
2015-06-29 22:23:03 +0800

    加入ios hook

---
+++ scripts/ios-uninstall.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+'use strict';
+
+module.exports = function(context){
+    if(context.opts.cordova.platforms.indexOf('ios') === -1){
+        console.info('IOS platform has not been added.');
+        return;
+    }
+
+    console.info('setting umeng share plugin ...');
+    var path = context.requireCordovaModule('path'),
+        fs = context.requireCordovaModule('fs'),
+        projectRoot = context.opts.projectRoot,
+        config = new ConfigParser(path.join(projectRoot, 'config.xml')),
+        appName = config.name() || 'CordovaApp';
+
+    var targetFile = path.join(projectRoot, 'platforms', 'ios', appName, 'Classes', 'AppDelegate.m');
+
+    var content = fs.readFileSync(targetFile, {encoding: 'utf8'});
+    if(content.indexOf('UMSocialSnsService.h') > -1){
+        content = content.replace('#import "UMSocialSnsService.h"', '');
+        content = content.replace('\n- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url{\n    return [UMSocialSnsService handleOpenURL:url];\n}', '');
+
+        fs.writeFileSync(targetFile, content);
+    }
+
+};
