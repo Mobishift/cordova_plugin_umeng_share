028fe256b8aa7fc05fa244ead37a2fe7f22096dc
Zhengz <zhengz@masn.cn>
2015-06-29 12:16:10 +0800

    微信分享集成

--- src/ios/UMengShare.m
+++ src/ios/UMengShare.m
@@ -2,10 +2,11 @@
 
 #import <Cordova/CDV.h>
 #import "UMSocial.h"
+#import "UMSocialWechatHandler.h"
 
 static BOOL isInit = false;
 
-@interface UMengShare : CDVPlugin {
+@interface UMengShare : CDVPlugin <UMSocialUIDelegate>{
   // Member variables go here.
 }
 
@@ -16,22 +17,52 @@ static BOOL isInit = false;
 
 - (void)share:(CDVInvokedUrlCommand*)command
 {
+    [self initShare];
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
+    NSData* shareData = [echo dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary* shareModel = [NSJSONSerialization JSONObjectWithData:shareData options:NSJSONReadingMutableLeaves error:nil];
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    CDVViewController* viewController = (CDVViewController*)self.viewController;
+    NSString* wechatAppId = [viewController.settings objectForKey:@"wechatappid"];
+    NSString* wechatAppSecret = [viewController.settings objectForKey:@"wechatappsecret"];
+
+    NSMutableArray* shareNames = [NSMutableArray array];
+    if(wechatAppId != nil && wechatAppSecret != nil){
+        [UMSocialWechatHandler setWXAppId:wechatAppId appSecret:wechatAppSecret url:[shareModel objectForKey:@"url"]];
+        [shareNames addObject:UMShareToWechatSession];
+        [shareNames addObject:UMShareToWechatTimeline];
     }
+
+    NSString* imageUrl = [shareModel objectForKey:@"image"];
+    UIImage* image = nil;
+    if(imageUrl != nil){
+        image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageUrl]]];
+
+    }
+    [UMSocialSnsService presentSnsIconSheetView:viewController
+                                         appKey:[viewController.settings objectForKey:@"umengappkey"]
+                                      shareText:[shareModel objectForKey:@"title"]
+                                     shareImage:image
+                                shareToSnsNames:shareNames
+                                       delegate:self];
+
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)initShare{
     if(!isInit){
-        
+        CDVViewController* viewController = (CDVViewController*)self.viewController;
+        [UMSocialData setAppKey:[viewController.settings objectForKey:@"umengappkey"]];
     }
 }
+
+- (void)didFinishGetUMSocialDataInViewController:(UMSocialResponseEntity *)response{
+    if(response.responseCode == UMSResponseCodeSuccess){
+         NSLog(@"share to sns name is %@",[[response.data allKeys] objectAtIndex:0]);
+    }
+}
+
 
 @end
